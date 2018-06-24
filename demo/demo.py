1b2620500755c1125464cf6fc1644d43a5a47684
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-06-23 20:00:04 -0700

    Added

--- demo/demo.py
+++ demo/demo.py
@@ -8,7 +8,7 @@ from pprint import pprint
 
 # create an instance of the API class
 api_instance = cloudmersive_nlp_api_client.ExtractEntitiesStringApi()
-value = 'A co-owner of the Red Hen in Lexington, Virginia, asked Ms Sanders and her family to leave as a protest against the Trump administration.' # str | Input string
+value = 'Cloudmersive is a leader in Highly Scalable Cloud APIs.' # str | Input string
 
 api_instance.api_client.configuration.api_key = {}
 api_instance.api_client.configuration.api_key['Apikey'] = '04d1a7be-c9d1-4d93-8ec4-e7545c2a570a'
